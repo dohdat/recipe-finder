019091367e2a1cee6533b80fd2f9e037fa39f0c5
Guillermo Gette <guillermogette@gmail.com>
2015-03-30 23:54:19 +1100

    add test for set expiration

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -7,4 +7,26 @@ class ItemTest extends \PHPUnit_Framework_TestCase
 		$item->setName('test_name');
 		$this->assertEquals('test_name',$item->getName());
 	}
+
+	public function testSetExpiration()
+	{
+		date_default_timezone_set('Australia/Sydney'); 
+
+		$item = new Recipe_Finder\Item();
+		$item->setExpiration('30/03/2015');
+		$this->assertEquals('1427634000',$item->getExpiration());
+	}
+
+	/**
+     * @expectedException        \Exception
+     * @expectedExceptionMessage Expiration date should be in the following format dd/mm/yyyy
+     */
+    public function testSetExpirationException()
+    {
+        date_default_timezone_set('Australia/Sydney'); 
+
+		$item = new Recipe_Finder\Item();
+		$item->setExpiration('asdas/asdas/2015');
+		$item->getExpiration();
+    }
 }
