843a3526e20a89b8f0ebbcdf1dc47e3ba9e43787
Guillermo Gette <guillermogette@gmail.com>
2015-03-31 22:13:04 +1100

    fix functions

--- src/Fridge.php
+++ src/Fridge.php
@@ -8,27 +8,25 @@ class Fridge
 	public function loadFridge($file = "")
 	{
 		if (empty($file)) {
-			throw new Exception("Filename can't be empty");
+			throw new \Exception("Filename can't be empty");
 			return;
 		}
-		$data = $getCSVData($file);
+		$data = $this->loadCSVData($file);
 	}
 
-	public function getCSVData($file = "")
+	public function loadCSVData($file = "")
 	{
-		$file_handle = @fopen($file, "r");
+		$file_handle = fopen($file, "r");
 		if ($file_handle === false) {
-			throw new Exception("The file couldn't be open");
+			throw new \Exception("The file couldn't be open");
 			return;
 		}
-
-		$data = fgetcsv($file_handle);
-		if (is_null($data) || $data === false) {
-			throw new Exception("The file is empty or couldnt be formatted");
-			return;
+		$data = array();
+		while ($line = fgetcsv($file_handle)) {
+			$data[] = $line;
 		}
-		
 		fclose($file_handle);
+
 		return $data;
 	}
 }
