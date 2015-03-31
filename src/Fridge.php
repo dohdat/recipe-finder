769d4f79a69f11e6b8d6eeac32f96e13fa723b88
Guillermo Gette <guillermogette@gmail.com>
2015-03-31 21:37:46 +1100

    basic fridge methods

--- src/Fridge.php
+++ src/Fridge.php
@@ -1,6 +1,34 @@
 <?php
 namespace Recipe_Finder;
 
-class Fridge{
+class Fridge
+{
+	protected $items;
 
+	public function loadFridge($file = "")
+	{
+		if (empty($file)) {
+			throw new Exception("Filename can't be empty");
+			return;
+		}
+		$data = $getCSVData($file);
+	}
+
+	public function getCSVData($file = "")
+	{
+		$file_handle = @fopen($file, "r");
+		if ($file_handle === false) {
+			throw new Exception("The file couldn't be open");
+			return;
+		}
+
+		$data = fgetcsv($file_handle);
+		if (is_null($data) || $data === false) {
+			throw new Exception("The file is empty or couldnt be formatted");
+			return;
+		}
+		
+		fclose($file_handle);
+		return $data;
+	}
 }
