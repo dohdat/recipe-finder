0ff5a8923d0031cc5ed77f53a70f7cb6e6b06a9d
Guillermo Gette <guillermogette@gmail.com>
2015-03-31 23:37:28 +1100

    add basic methods for recipe

--- src/Recipe.php
+++ src/Recipe.php
@@ -1,6 +1,26 @@
 <?php
 namespace Recipe_Finder;
 
-class Recipe{
-	
+class Recipe
+{
+	protected $name;
+	protected $ingredients = array();
+
+	public function setName($name = "")
+	{
+		if (empty($name)) {
+			throw new \Exception("Name can't be empty");
+		} else {
+			$this->name = $name;
+		}
+	}
+
+	public function addIngredient($name = "", $amount = 0, $unit = "")
+	{
+		$item = new Item();
+		$item->setName($name);
+		$item->setAmount($amount);
+		$item->setUnit($unit);
+		$this->ingredients[] = $item;
+	}
 }
