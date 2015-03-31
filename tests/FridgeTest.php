7f5fabc51c41b41da6d4efb8e510c06cef06f410
Guillermo Gette <guillermogette@gmail.com>
2015-03-31 22:33:32 +1100

    add fridge test

--- tests/FridgeTest.php
+++ tests/FridgeTest.php
@@ -10,4 +10,17 @@ class FridgeTest extends \PHPUnit_Framework_TestCase
 		$file_data = $fridge->loadCSVData('tests/fridge.csv');
 		$this->assertEquals($data, $file_data);
 	}
+
+	function testLoadFridge()
+	{
+		$fridge = new Recipe_Finder\Fridge();
+		$fridge->loadFridge('tests/fridge.csv');
+		$item = new Recipe_Finder\Item();
+		$item->setName('bread');
+		$item->setAmount('10');
+		$item->setUnit('slices');
+		$item->setExpiration('25/12/2014');
+		$items = array($item);
+		$this->assertEquals($items, $fridge->getItems());
+	}
 }
