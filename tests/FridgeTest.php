e9f1cf67d84d2770838d19f8c71348062e2ef70d
Guillermo Gette <guillermogette@gmail.com>
2015-03-31 23:20:04 +1100

    change function name

--- tests/FridgeTest.php
+++ tests/FridgeTest.php
@@ -14,7 +14,7 @@ class FridgeTest extends \PHPUnit_Framework_TestCase
 	function testLoadFridge()
 	{
 		$fridge = new Recipe_Finder\Fridge();
-		$fridge->loadFridge('tests/fridge.csv');
+		$fridge->loadFridgeFile('tests/fridge.csv');
 		$item = new Recipe_Finder\Item();
 		$item->setName('bread');
 		$item->setAmount('10');
@@ -28,7 +28,7 @@ class FridgeTest extends \PHPUnit_Framework_TestCase
 	function testHas()
 	{
 		$fridge = new Recipe_Finder\Fridge();
-		$fridge->loadFridge('tests/fridge.csv');
+		$fridge->loadFridgeFile('tests/fridge.csv');
 		$this->assertEquals(false, $fridge->has('cheese','10'));
 		$this->assertEquals(false, $fridge->has('bread','19'));
 		$this->assertEquals(true, $fridge->has('bread','8'));
